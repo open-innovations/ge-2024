04d0a1924f0c84050531e531205c0f2106bb442b
Giles Dring <giles@dringtech.com>
2024-07-01 13:02:45 +0100

    Update display of results on constituency page

--- src/_components/party.ts
+++ src/_components/party.ts
@@ -1,9 +1,11 @@
 import { Colour } from "https://deno.land/x/oi_lume_viz@v0.15.6/lib/colour/colour.ts";
 
-export default function ({ party, name, parties }: Lume.Data) {
+export default function ({ party, name, parties, tag }: Lume.Data) {
   const key = Object.keys(parties).includes(party) ? party : "other";
   const c = Colour(key);
-  return `<span style="color:${c.contrast};background:${c.hex};padding:0.2em 0.5em;">${
+  return `<${
+    tag || "span"
+  } style="color:${c.contrast};background:${c.hex};padding:0.2em 0.5em;">${
     name || parties[key]?.name
-  }</span>`;
+  }</${tag || "span"}>`;
 }
