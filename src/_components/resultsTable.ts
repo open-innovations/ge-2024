d9a2a16e44eac7dfa2b9856b82ea83bdf632a02f
Giles Dring <giles@dringtech.com>
2024-07-02 14:24:55 +0100

    Fix linting / formatting

--- src/_components/resultsTable.ts
+++ src/_components/resultsTable.ts
@@ -1,53 +1,60 @@
 import { Colour } from "https://deno.land/x/oi_lume_viz@v0.15.6/lib/colour/colour.ts";
 
-export default function ({ results, parties, thumbnails, tag }: Lume.Data) {
-	let pts = Object.keys(parties);
-	let html = '<table class="results"><tbody>';
-	let r,i,key,party,c,src,total,pc,w,v;
-	
-	if(typeof thumbnails==="undefined") thumbnails = {};
-	
-	total = (results.total_votes||0);
-	if(total==0){
-		for(i = 0; i < results.votes.length; i++){
-			if(typeof results.votes[i].votes==="number" && results.votes[i].votes > 0){
-				total += results.votes[i].votes;
-			}
-		}
-	}
+export default function ({ results, parties, thumbnails }: Lume.Data) {
+  const pts = Object.keys(parties);
+  let html = '<table class="results"><tbody>';
+  let r, i, key, party, c, src, total, pc, w, v;
 
-	for(i = 0; i < results.votes.length; i++){
-		r = results.votes[i];
-		party = r.party_key;
-		key = pts.includes(party) ? party : "other";
-		c = Colour(key);
-		v = (r.votes||0);
-		if(total > 0){
-			pc = (100 * v / total).toFixed(1) + '%';
-			w = (80 * v / total).toFixed(1);
-			v = v.toLocaleString() + ' votes';
-		}else{
-			pc = "TBA";
-			w = "0";
-			v = "";
-		}
-		
-		html += '<tr title="'+r.person_name+' ('+r.party_name+'): ' + v + (v ? '(' + pc + ')' : pc) + '" data-party="' + r.party_key + '">';
-		html += '<td>';
-		src = "/assets/images/missing.svg";
-		if(typeof thumbnails[r.person_id]==="string"){
-			src = thumbnails[r.person_id]; // r.image
-		}
-		html += '<a href="https://whocanivotefor.co.uk/person/' + r.person_id + '/" title="' + r.person_name + '"><img src="' + src + '" alt="Photograph of ' + r.person_name + '"/></a>';
-		html += '</td>';
-		html += '<td>';
-		html += '<span class="party-bar" style="width:' + w + '%;background:' + c.hex + ';color:' + c.contrast + ';"></span>';
-		html += '<span class="candidate">' + r.person_name + '</span>'
-		html += ' / <span class="party">' + parties[r.party_key].pa + '</span><br />';
-		html += ' <strong class="percent">' + pc + '</strong>';
-		html += '</td>';
-		html += '</tr>'
-	}
-	html += '</table>';
-	return html;
+  if (typeof thumbnails === "undefined") thumbnails = {};
+
+  total = results.total_votes || 0;
+  if (total == 0) {
+    for (i = 0; i < results.votes.length; i++) {
+      if (
+        typeof results.votes[i].votes === "number" && results.votes[i].votes > 0
+      ) {
+        total += results.votes[i].votes;
+      }
+    }
+  }
+
+  for (i = 0; i < results.votes.length; i++) {
+    r = results.votes[i];
+    party = r.party_key;
+    key = pts.includes(party) ? party : "other";
+    c = Colour(key);
+    v = r.votes || 0;
+    if (total > 0) {
+      pc = (100 * v / total).toFixed(1) + "%";
+      w = (80 * v / total).toFixed(1);
+      v = v.toLocaleString() + " votes";
+    } else {
+      pc = "TBA";
+      w = "0";
+      v = "";
+    }
+
+    html += '<tr title="' + r.person_name + " (" + r.party_name + "): " + v +
+      (v ? "(" + pc + ")" : pc) + '" data-party="' + r.party_key + '">';
+    html += "<td>";
+    src = "/assets/images/missing.svg";
+    if (typeof thumbnails[r.person_id] === "string") {
+      src = thumbnails[r.person_id]; // r.image
+    }
+    html += '<a href="https://whocanivotefor.co.uk/person/' + r.person_id +
+      '/" title="' + r.person_name + '"><img src="' + src +
+      '" alt="Photograph of ' + r.person_name + '"/></a>';
+    html += "</td>";
+    html += "<td>";
+    html += '<span class="party-bar" style="width:' + w + "%;background:" +
+      c.hex + ";color:" + c.contrast + ';"></span>';
+    html += '<span class="candidate">' + r.person_name + "</span>";
+    html += ' / <span class="party">' + parties[r.party_key].pa +
+      "</span><br />";
+    html += ' <strong class="percent">' + pc + "</strong>";
+    html += "</td>";
+    html += "</tr>";
+  }
+  html += "</table>";
+  return html;
 }
--- src/_data.js
+++ src/_data.js
@@ -1,145 +1,148 @@
-export function buildLegend(data,column,labelcol,labels={}){
+export function buildLegend(data, column, labelcol, labels = {}) {
+  // From https://election.pressassociation.com/general-election/general-election-2024/
+  const pacodes = {
+    "con": "Con",
+    "lab": "Lab",
+    "ld": "LD",
+    "pc": "PC",
+    "snp": "SNP",
+    "green": "Green",
+    "ref": "Reform",
+    "ind": "Ind",
+    "dup": "DUP",
+    "sf": "SF",
+    "sdlp": "SDLP",
+    "apni": "Alliance",
 
-	// From https://election.pressassociation.com/general-election/general-election-2024/
-	let pacodes = {
-		"con":"Con",
-		"lab":"Lab",
-		"ld":"LD",
-		"pc":"PC",
-		"snp":"SNP",
-		"green":"Green",
-		"ref":"Reform",
-		"ind":"Ind",
-		"dup":"DUP",
-		"sf":"SF",
-		"sdlp":"SDLP",
-		"apni":"Alliance",
+    "Conservative": "Con",
+    "Labour": "Lab",
+    "Liberal Democrat": "LD",
+    "Plaid Cymru": "PC",
+    "Scottish National Party": "SNP",
+    "Green": "Green",
+    "Green Party": "Green",
+    "UK Independence Party": "UKIP",
+    "Democratic Unionist Party": "DUP",
+    "Ulster Unionist Party": "UUP",
+    "Social Democratic and Labour Party": "SDLP",
+    "Alba": "Alba",
+    "Sinn Féin": "SF",
+    "Alliance": "Alliance",
+    "Reform UK": "Reform",
+    "Workers Party of Britain": "WPB",
+    "Abolish the Welsh Assembly Party": "Abolish",
+    "Alliance for Democracy and Freedom": "ADF",
+    "Alliance for Green Socialism": "Green Soc",
+    "Animal Welfare Party": "AWP",
+    "Aontu": "Aontu",
+    "Ashfield Independents": "Ashfield",
+    "Blue Revolution": "Blue",
+    "British Democratic Party": "BDP",
+    "British Unionist Party": "BUP",
+    "Chesterfield Independents": "Chesterfield",
+    "Christian Party": "Christian",
+    "Christian Peoples Alliance": "CPA",
+    "Climate": "Climate",
+    "Communist Future": "Comm Future",
+    "Communist League": "Comm Lge",
+    "Communist Party of Britain": "Comm Brit",
+    "Confelicity": "Confelicity",
+    "Consensus": "Consensus",
+    "Count Binface Party": "Binface",
+    "Cross-Community Labour Alternative": "CCLA",
+    "Democracy for Chorley": "Chorley",
+    "English Constitution Party": "ECP",
+    "English Democrats": "Eng Dem",
+    "Fairer Voting Party": "FVP",
+    "Freedom Alliance": "FA",
+    "Hampshire Independents": "HampInd",
+    "Heritage Party": "Heritage",
+    "Independence for Scotland": "Ind Scot",
+    "Independents for Direct Democracy": "IDD",
+    "Kingston Independent Residents Group": "KIRG",
+    "Liberal": "Lib",
+    "Libertarian Party": "Libertarian",
+    "Lincolnshire Independents": "Lincs",
+    "Liverpool Community Independents": "LCI",
+    "Monster Raving Loony Party": "Loony",
+    "National Health Action Party": "NHAP",
+    "New Open Non-Political Organised Leadership": "NonPol",
+    "One Leicester": "One Leicester",
+    "Party of Women": "PartyWomen",
+    "People Before Profit": "PBP",
+    "Portsmouth Independent Party": "Portsmouth",
+    "Propel": "Propel",
+    "Psychedelic Future Party": "PFP",
+    "Putting Crewe First": "Crewe",
+    "Rebooting Democracy": "Rebooting",
+    "Rejoin EU": "Rejoin",
+    "Save Us Now": "Save",
+    "Scottish Christian Party": "SCP",
+    "Scottish Family Party": "Scot Family",
+    "Scottish Libertarian Party": "SLP",
+    "Scottish Socialist Party": "SSP",
+    "Shared Ground": "SG",
+    "Social Democratic Party": "Soc Dem",
+    "Social Democratic &amp; Labour Party": "Soc Dem",
+    "Social Justice Party": "SJP",
+    "Socialist Equality": "Soc Eq",
+    "Socialist Labour Party": "Soc Lab",
+    "Socialist Party of Great Britain": "SPGB",
+    "South Devon Alliance": "Devon",
+    "Sovereignty": "Sovereignty",
+    "Stockport Fights Austerity No to Cuts": "Stockport",
+    "Swale Indpendents": "Swale",
+    "Taking The Initiative Party": "TTIP",
+    "The Common Good": "Good",
+    "The Common People": "People",
+    "The Mitre TW9": "Mitre",
+    "The North East Party": "NE Party",
+    "The Peace Party": "Peace",
+    "The Speaker": "Speaker",
+    "speaker": "Speaker",
+    "The Yorkshire Party": "Yorkshire",
+    "The Yoruba Party in the UK": "Yoruba",
+    "Trade Unionist and Socialist Coalition": "TUSC",
+    "Traditional Unionist Voice": "TUV",
+    "Transform": "Transform",
+    "True & Fair": "True",
+    "UK Voice": "Voice",
+    "Volt": "Volt",
+    "Women’s Equality Party": "Women",
+    "Workers’ Revolutionary Party": "WRP",
+  };
 
-		"Conservative":"Con",
-		"Labour":"Lab",
-		"Liberal Democrat":"LD",
-		"Plaid Cymru":"PC",
-		"Scottish National Party":"SNP",
-		"Green":"Green",
-		"Green Party":"Green",
-		"UK Independence Party":"UKIP",
-		"Democratic Unionist Party":"DUP",
-		"Ulster Unionist Party":"UUP",
-		"Social Democratic and Labour Party":"SDLP",
-		"Alba":"Alba",
-		"Sinn Féin":"SF",
-		"Alliance":"Alliance",
-		"Reform UK":"Reform",
-		"Workers Party of Britain":"WPB",
-		"Abolish the Welsh Assembly Party":"Abolish",
-		"Alliance for Democracy and Freedom":"ADF",
-		"Alliance for Green Socialism":"Green Soc",
-		"Animal Welfare Party":"AWP",
-		"Aontu":"Aontu",
-		"Ashfield Independents":"Ashfield",
-		"Blue Revolution":"Blue",
-		"British Democratic Party":"BDP",
-		"British Unionist Party":"BUP",
-		"Chesterfield Independents":"Chesterfield",
-		"Christian Party":"Christian",
-		"Christian Peoples Alliance":"CPA",
-		"Climate":"Climate",
-		"Communist Future":"Comm Future",
-		"Communist League":"Comm Lge",
-		"Communist Party of Britain":"Comm Brit",
-		"Confelicity":"Confelicity",
-		"Consensus":"Consensus",
-		"Count Binface Party":"Binface",
-		"Cross-Community Labour Alternative":"CCLA",
-		"Democracy for Chorley":"Chorley",
-		"English Constitution Party":"ECP",
-		"English Democrats":"Eng Dem",
-		"Fairer Voting Party":"FVP",
-		"Freedom Alliance":"FA",
-		"Hampshire Independents":"HampInd",
-		"Heritage Party":"Heritage",
-		"Independence for Scotland":"Ind Scot",
-		"Independents for Direct Democracy":"IDD",
-		"Kingston Independent Residents Group":"KIRG",
-		"Liberal":"Lib",
-		"Libertarian Party":"Libertarian",
-		"Lincolnshire Independents":"Lincs",
-		"Liverpool Community Independents":"LCI",
-		"Monster Raving Loony Party":"Loony",
-		"National Health Action Party":"NHAP",
-		"New Open Non-Political Organised Leadership":"NonPol",
-		"One Leicester":"One Leicester",
-		"Party of Women":"PartyWomen",
-		"People Before Profit":"PBP",
-		"Portsmouth Independent Party":"Portsmouth",
-		"Propel":"Propel",
-		"Psychedelic Future Party":"PFP",
-		"Putting Crewe First":"Crewe",
-		"Rebooting Democracy":"Rebooting",
-		"Rejoin EU":"Rejoin",
-		"Save Us Now":"Save",
-		"Scottish Christian Party":"SCP",
-		"Scottish Family Party":"Scot Family",
-		"Scottish Libertarian Party":"SLP",
-		"Scottish Socialist Party":"SSP",
-		"Shared Ground":"SG",
-		"Social Democratic Party":"Soc Dem",
-		"Social Democratic &amp; Labour Party":"Soc Dem",
-		"Social Justice Party":"SJP",
-		"Socialist Equality":"Soc Eq",
-		"Socialist Labour Party":"Soc Lab",
-		"Socialist Party of Great Britain":"SPGB",
-		"South Devon Alliance":"Devon",
-		"Sovereignty":"Sovereignty",
-		"Stockport Fights Austerity No to Cuts":"Stockport",
-		"Swale Indpendents":"Swale",
-		"Taking The Initiative Party":"TTIP",
-		"The Common Good":"Good",
-		"The Common People":"People",
-		"The Mitre TW9":"Mitre",
-		"The North East Party":"NE Party",
-		"The Peace Party":"Peace",
-		"The Speaker":"Speaker",
-		"speaker":"Speaker",
-		"The Yorkshire Party":"Yorkshire",
-		"The Yoruba Party in the UK":"Yoruba",
-		"Trade Unionist and Socialist Coalition":"TUSC",
-		"Traditional Unionist Voice":"TUV",
-		"Transform":"Transform",
-		"True & Fair":"True",
-		"UK Voice":"Voice",
-		"Volt":"Volt",
-		"Women’s Equality Party":"Women",
-		"Workers’ Revolutionary Party": "WRP"
-	}
+  const lookup = {};
+  for (let r = 0; r < data.length; r++) {
+    const v = data[r][column];
+    if (v) {
+      if (!lookup[v]) lookup[v] = { "count": 0, "label": v };
+      lookup[v].count++;
+      if (labelcol && labelcol in data[r]) lookup[v].label = data[r][labelcol];
+    }
+  }
 
-	var lookup = {};
-	for(let r = 0; r < data.length; r++){
-		let v = data[r][column];
-		if(v){
-			if(!lookup[v]) lookup[v] = {'count':0,'label':v};
-			lookup[v].count++;
-			if(labelcol && labelcol in data[r]) lookup[v].label = data[r][labelcol];
-		}
-	}
+  const legend = [];
+  for (const party in lookup) {
+    let lbl = lookup[party].label;
+    if (lbl in labels) lbl = labels[lbl];
+    if (party in labels) lbl = labels[party];
+    if (lbl in pacodes) lbl = pacodes[lbl];
+    else console.warn("No match for " + lbl + " (" + party + ")");
+    legend.push({
+      "colour": party,
+      "count": lookup[party].count,
+      "label": lbl + ": " + lookup[party].count,
+    });
+  }
+  legend.sort(function (a, b) {
+    if (a.colour == "Spk") return -1;
+    if (b.colour == "Spk") return 1;
+    // Sort by count
+    if (a.count - b.count != 0) return (a.count - b.count);
+    // Fall back to sorting by label
+    return (a.label.toLowerCase() < b.label.toLowerCase() ? 1 : -1);
+  });
 
-	let legend = [];
-	for(let party in lookup){
-		let lbl = lookup[party].label; 
-		if(lbl in labels) lbl = labels[lbl];
-		if(party in labels) lbl = labels[party];
-		if(lbl in pacodes) lbl = pacodes[lbl];
-		else console.warn('No match for '+lbl+' ('+party+')');
-		legend.push({'colour':party,'count':lookup[party].count,'label':lbl+': '+lookup[party].count});
-	}
-	legend.sort(function(a, b){
-		if(a.colour=="Spk") return -1;
-		if(b.colour=="Spk") return 1;
-		// Sort by count
-		if(a.count - b.count != 0) return (a.count - b.count);
-		// Fall back to sorting by label
-		return (a.label.toLowerCase() < b.label.toLowerCase() ? 1 : -1);
-	});
-
-	return legend;
+  return legend;
 }
