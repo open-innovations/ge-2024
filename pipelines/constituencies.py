3433d3da7bd7d2810dfe2d3a31ec128e8e5568a7
luke-strange <92686634+luke-strange@users.noreply.github.com>
2024-07-04 14:42:05 +0100

    Store drilldown data as JSON

--- pipelines/constituencies.py
+++ pipelines/constituencies.py
@@ -34,6 +34,10 @@ def write_csv(data, path):
     data.to_csv(path)
     return 
 
+def write_json(data, path):
+    data.to_json(path, orient='index', indent=2)
+    return 
+
 def get_data(sheet_name, var_name, drop_cols, value_type=int, pct=None):
     # Set params
     path = 'data/Demographic-data-for-new-parliamentary-constituencies-May-2024.xlsx'
@@ -45,10 +49,10 @@ def get_data(sheet_name, var_name, drop_cols, value_type=int, pct=None):
 
     # Drop unused columns and rename to merge with constituency codes
     data.drop(columns=drop_cols, inplace=True)
-    data.rename(columns={'New constituency name': 'PCON24NM', 'Constituency value': 'value'}, inplace=True)
+    data.rename(columns={'New constituency name': 'PCON24NM', 'Constituency value': var_name}, inplace=True)
 
     # Convert the data types to integers
-    data['value'] = data['value'].astype(value_type)
+    data[var_name] = data[var_name].astype(value_type)
 
     # Get a list of unique variable names, and how many there are.
     v_names = list(data['Variable'].unique())
@@ -57,7 +61,7 @@ def get_data(sheet_name, var_name, drop_cols, value_type=int, pct=None):
     if v_num > 1:
             # If there are multiple variables for a given topic, we want to pivot the data so that each of of these has its own column.
             print(f'More than one variable type: {v_names}. Pivoting the data\n')
-            data = data.pivot(index='PCON24NM', columns='Variable', values='value')
+            data = data.pivot(index='PCON24NM', columns='Variable', values=var_name)
 
     if v_num == 1:
         # If there is only one variable, we can drop the variable column as we don't need it.
@@ -71,6 +75,9 @@ def get_data(sheet_name, var_name, drop_cols, value_type=int, pct=None):
         # If the data is a pct, make it out of 100 and round to 1dp.
         data[v_names] = data[v_names].mul(100).round(1)
 
+    # Drop PCON24NM column
+    data.drop(columns='PCON24NM', inplace=True)
+
     return data
 
 def concatenate_nations_data(dataframes):
@@ -82,19 +89,23 @@ if __name__ == "__main__":
     england_wales_population = get_data('EW_data', 'Population', ['Topic', 'England & Wales value'])
     northern_ireland_population = get_data('NI_data', 'Population', ['Topic', 'Northern Ireland value'])
     uk_population = concatenate_nations_data([england_wales_population, northern_ireland_population])
-    write_csv(uk_population, 'src/_data/drilldown/population.csv')
+    
 
     ew_age = get_data('EW_data', 'Age', ['Topic', 'England & Wales value'], value_type=float, pct=True)
     ni_age = get_data('NI_data', 'Age', ['Topic', 'Northern Ireland value'], value_type=float, pct=True)
     uk_age = concatenate_nations_data([ew_age, ni_age])
-    write_csv(uk_age, 'src/_data/drilldown/age.csv')
+    
 
     ew_housing_tenure = get_data('EW_data', 'Housing tenure', ['Topic', 'England & Wales value'], value_type=float, pct=True)
     ni_housing_tenure = get_data('NI_data', 'Housing tenure', ['Topic', 'Northern Ireland value'], value_type=float, pct=True)
     uk_housing_tenure = concatenate_nations_data([ew_housing_tenure, ni_housing_tenure])
-    write_csv(uk_housing_tenure, 'src/_data/drilldown/housing_tenure.csv')
+    
 
     ew_households = get_data('EW_data', 'Households', ['Topic', 'England & Wales value'], value_type=int)
     ni_households = get_data('NI_data', 'Households', ['Topic', 'Northern Ireland value'], value_type=int)
     uk_households = concatenate_nations_data([ew_households, ni_households])
-    write_csv(uk_households, 'src/_data/drilldown/households.csv')
+
+    constituency_drilldown = uk_age.join([uk_households, uk_population, uk_housing_tenure])
+    constituency_drilldown = constituency_drilldown.merge(geo_codes, on='PCON24CD')
+    constituency_drilldown.set_index('PCON24CD', inplace=True)
+    write_json(constituency_drilldown, 'src/_data/drilldown.json')
